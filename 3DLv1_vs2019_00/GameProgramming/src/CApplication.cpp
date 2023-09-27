9b0921948424b9873f749cc79be98fa946680c05
Mouri-Masakiyo-Fukuyama <gc23001@anabuki-net.ne.jp>
2023-09-27 09:27:13 +0900

    課題4

--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
@@ -13,6 +13,8 @@ CCharacterManager CApplication::mCharacterManager;
 #define SOUND_BGM "res\\mario.wav" //BGM音声ファイル
 #define SOUND_OVER "res\\mdai.wav" //ゲームオーバー音声ファイル
 
+CVector mEye;
+
 CCharacterManager* CApplication::CharacterManager()
 {
 	return &mCharacterManager;
@@ -25,6 +27,7 @@ CTexture* CApplication::Texture()
 
 void CApplication::Start()
 {
+	mEye = CVector(1.0f, 2.0f, 3.0f);
 }
 
 void CApplication::Update()
@@ -40,9 +43,33 @@ void CApplication::Update()
 	//頂点3の座標を設定する
 	v2.Set(0.0f, 0.0f, -0.5f);
 
+	if (mInput.Key('J'))
+	{
+		mEye = mEye - CVector(0.1f, 0.0f, 0.0f);
+	}
+	if (mInput.Key('L'))
+	{
+		mEye = mEye + CVector(0.1f, 0.0f, 0.0f);
+	}
+	if (mInput.Key('I'))
+	{
+		mEye = mEye - CVector(0.0f, 0.0f, 0.1f);
+	}
+	if (mInput.Key('K'))
+	{
+		mEye = mEye + CVector(0.0f, 0.0f, 0.1f);
+	}
+	if (mInput.Key('M'))
+	{
+		mEye = mEye - CVector(0.0f, 0.1f, 0.0f);
+	}
+	if (mInput.Key('O'))
+	{
+		mEye = mEye + CVector(0.0f, 0.1f, 0.0f);
+	}
 	//視点の設定
-	//gluLookAt(視点X,視点Y,視点Z,中心X,中心Y,中心Z,上向X,上向Y,上向Z)
-	gluLookAt(1.0f, 2.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	//gluLookAt(視点X,視点Y,視点Z,中心X,中心Y,中心Z,上向X,上向Y,上向Z)              
+	gluLookAt(mEye.X(), mEye.Y(), mEye.Z(), 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
 
 	//描画開始
 	glBegin(GL_TRIANGLES);
--- 3DLv1_vs2019_00/GameProgramming/src/CVector.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CVector.cpp
@@ -23,16 +23,28 @@ float CVector::Z() const
 }
 
 CVector::CVector()
-{
-	mX = (0.0f);
-	mY = (0.0f);
-	mZ = (0.0f);
-}
+	:mX(0.0f)
+	,mY(0.0f)
+	,mZ(0.0f)
+{}
 
 
 CVector::CVector(float x, float y, float z)
+	:mX(x)
+	,mY(y)
+	,mZ(z)
+{}
+
+//+演算子のオーバーロード
+//CVector + CVectorの演算結果を返す
+CVector CVector::operator+(const CVector& v)const
 {
-	mX = x;
-	mY = y;
-	mZ = z;
+	return CVector(mX + v.mX, mY + v.mY, mZ + v.mZ);
+}
+
+//-演算子のオーバーロード
+//CVector - CVectorの演算結果を返す
+CVector CVector::operator-(const CVector& v)const
+{
+	return CVector(mX - v.mX, mY - v.mY, mZ - v.mZ);
 }
