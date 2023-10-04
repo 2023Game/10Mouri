f58d00f0988835dd794d7b2a6be9c49887a55364
Mouri-Masakiyo-Fukuyama <gc23001@anabuki-net.ne.jp>
2023-10-04 09:12:59 +0900

    課題7

--- 3DLv1_vs2019_00/GameProgramming/src/CModel.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CModel.h
@@ -1,15 +1,23 @@
 #pragma once
 #ifndef CMODEL_H
 #define CMODEL_H
+//vectorのインクルード
+#include <vector>
+#include "CTriangle.h"
 /*
 モデルクラス
 モデルデータの入力や表示
 */
 class CModel {
+private:
+	//三角形の可変長配列
+	std::vector<CTriangle> mTriangles;
 public:
 	//モデルファイルの入力
 	//Load(モデルファイル名,マテリアルファイル名)
 	void Load(char* obj, char* mtl);
+    //描画
+	void Render();
 };
 
 #endif
