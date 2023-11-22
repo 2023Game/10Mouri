30010ac9a4b6d0bf1b873eabfb456363e8011c40
Mouri-Masakiyo-Fukuyama <gc23001@anabuki-net.ne.jp>
2023-11-22 10:09:33 +0900

    課題19

--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.cpp
@@ -76,7 +76,7 @@ void CApplication::Update()
 	//カメラのパラメータを作成する
 	CVector e, c, u;//視点,注視点,上方向
 	//視点を求める
-	e =
+	e = mPlayer.Position() + CVector(0.0f, 1.0f, -3.0f) * mPlayer.MatrixRotate();
 	//注視点を求める
 	c = mPlayer.Position();
 	//上方向を求める
