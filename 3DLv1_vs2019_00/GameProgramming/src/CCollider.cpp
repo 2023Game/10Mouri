86e03fbd78920edf0e91a0cab0a748405a2ed80d
Mouri-Masakiyo-Fukuyama <gc23001@anabuki-net.ne.jp>
2023-12-05 12:42:54 +0900

    課題26

--- 3DLv1_vs2019_00/GameProgramming/src/CCollider.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CCollider.cpp
@@ -44,10 +44,12 @@ CCollider::~CCollider() {
 //Collision(コライダ1,コライダ2)
 //return:true(衝突している)false(衝突していない)
 bool CCollider::Collision(CCollider* m, CCollider* o) {
-	//書くこらいっだの中心座標を求める
+	//各コライダの中心座標を求める
 	//原点×コライダの変換行列×親の変換行列
 	CVector mpos = m->mPosition * *m->mpMatrix;
 	CVector opos = o->mPosition * *o->mpMatrix;
+	//中心から中心へのベクトルを求める
+	mpos = mpos - opos;
 	//中心の距離が半径の合計より小さいと衝突
 	if (m->mRadius + o->mRadius > mpos.Lenght()) {
 		//衝突している
