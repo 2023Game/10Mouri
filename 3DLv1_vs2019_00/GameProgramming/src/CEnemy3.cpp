780d5a57a95eca4741df6054a05cc6afde0b77f7
Mouri-Masakiyo-Fukuyama <gc23001@anabuki-net.ne.jp>
2024-04-04 18:12:10 +0900

    課題40

--- 3DLv1_vs2019_00/GameProgramming/src/CEnemy3.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CEnemy3.cpp
@@ -5,6 +5,7 @@
 
 #define OBJ "res\\f16.obj" //モデルのファイル
 #define MTL "res\\f16.mtl" //モデルのマテリアルファイル
+#define HP 3 //耐久力
 
 CModel CEnemy3::sModel;
 
@@ -12,6 +13,7 @@ CModel CEnemy3::sModel;
 CEnemy3::CEnemy3()
 	: CCharacter3(1)
 	, mCollider(this,&mMatrix,CVector(0.0f,0.0f,0.0f),0.4f)
+	, mHp(HP)
 {
 	//モデルが無いときは読み込む
 	if (sModel.Triangles().size() == 0)
@@ -69,6 +71,21 @@ void CEnemy3::Update()
 			}
 		}
 	}
+	//HPが0以下の時 撃破
+	if (mHp <= 0)
+	{
+		mHp--;
+		//15フレーム毎にエフェクト
+		if (mHp % 15 == 0)
+		{
+			//エフェクト生成
+			new CEffect(mPosition, 1.0f, 1.0f, "exp.tga", 4, 4, 2);
+		}
+		//下降させる
+		mPosition = mPosition - CVector(0.0f, 0.03f, 0.0f);
+		CTransform::Update();
+		return;
+	}
 }
 
 //衝突処理
@@ -79,6 +96,7 @@ void CEnemy3::Collision(CCollider* m, CCollider* o)
 	{
 	case CCollider::EType::ESPHERE:
 		if (CCollider::Collision(m, o)) {
+			mHp--; //ヒットポイントの減算
 			new CEffect(o->Parent()->Position(), 1.0f, 1.0f, "exp.tga", 4, 4, 2);
 		}
 		break;
@@ -86,6 +104,11 @@ void CEnemy3::Collision(CCollider* m, CCollider* o)
 		CVector adjust;
 		if (CCollider::CollisionTriangleSphere(o, m, &adjust))
 		{
+			//撃破で地面に衝突すると無効
+			if (mHp <= 0)
+			{
+				mEnabled = false;
+			}
 			mPosition = mPosition + adjust;
 		}
 		break;
