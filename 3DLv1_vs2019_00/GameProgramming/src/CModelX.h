e874c07b880bd1876f77f1d566082139a096dd66
Mouri-Masakiyo-Fukuyama <gc23001@anabuki-net.ne.jp>
2024-04-30 11:26:02 +0900

    3

--- 3DLv1_vs2019_00/GameProgramming/src/CModelX.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CModelX.h
@@ -1,5 +1,9 @@
 #ifndef CMODELX_H //インクルードガード
 #define CMODELX_H
+#include <vector> //vectorクラスのインクルード(動的配列)
+#include "CMatrix.h" //マトリクスのインクルード
+class CModelX; //CModelXクラスの宣言
+class CModelXFrame; //CModelXFrameクラスの宣言
 
 #define MODEL_FILE "res\\sample.blend.x" //入力ファイル名
 
@@ -10,17 +14,36 @@ CModelX
 Xファイル形式の3Dモデルデータをプログラムで認識する
 */
 class CModelX {
+	friend CModelXFrame;
 public:
+	~CModelX();
+	//ノードの読み飛ばし
+	void SkipNode();
 	//単語の取り出し
 	char* GetToken();
 	CModelX();
 	//ファイル読み込み
 	void Load(char* file);
 private:
+	std::vector<CModelXFrame*> mFrame; //フレームの配列
 	//cが区切り文字ならtrueを返す
 	bool IsDelimiter(char c);
 	char* mpPointer; //読み込み位置
 	char mToken[1024]; //取り出した単語の領域
+};
+//CModelXFrameクラスの定義
+class CModelXFrame {
+	friend CModelX;
+public:
+	//コンストラクタ
+	CModelXFrame(CModelX* model);
+	//デストラクタ
+	~CModelXFrame();
+private:
+	std::vector<CModelXFrame*> mChild; //子フレームの配列
+	CMatrix mTransformMatrix; //変換行列
+	char* mpName; //フレーム名前
+	int mIndex; //フレーム番号
 };
 
 #endif
